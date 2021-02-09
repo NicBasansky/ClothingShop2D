d745006dc28af081e7b3abec63a3c4908aad5e59
Nic Basansky <n_basansky@hotmail.com>
2021-02-08 22:53:58 -0800

    Setup a basic scene and introduced a character. Added movement and movement animation to player.

---
+++ ClothesShopProject/ClothesShop2D/Assets/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Shop.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] float speed = 2f;
+        Rigidbody2D rb;
+        Vector2 movement;
+        Animator anim;
+
+        private void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            movement.x = Input.GetAxis("Horizontal");
+            movement.y = Input.GetAxis("Vertical");
+
+            UpdateAnimations();
+        }
+
+        private void FixedUpdate()
+        {
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        }
+
+        private void UpdateAnimations()
+        {
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+            anim.SetFloat("Speed", movement.magnitude);
+        }
+
+    }
+}
+
